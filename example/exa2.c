778ac0281c4dd2445e13e9d0d1690364a6bb7d02
Tashfia Jannath <76718979+TashfiaJannath@users.noreply.github.com>
2021-03-04 20:51:01 +0800

    Create exa2.c

---
+++ example/exa2.c
@@ -0,0 +1,11 @@
+#include<conio.h>
+#include<stdio.h>
+int main(void)
+{
+    char ch;
+
+    printf("Enter a character: ");
+    ch=getche();
+    printf("\nIts ASCII code is %d", ch);
+    return 0;
+    }
