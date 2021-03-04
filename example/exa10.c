0a5f32e4f0540793abb6c6cba0bc5aa1ae46502b
Tashfia Jannath <76718979+TashfiaJannath@users.noreply.github.com>
2021-03-04 21:06:00 +0800

    Create exa10.c

---
+++ example/exa10.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void){
+    char ch;
+    printf("nter your message.\n");
+    ch=getche();
+
+    while(ch!='\r'){
+    printf("%c",ch=1);
+    ch=getche();}
+
+    return 0;
+    }
