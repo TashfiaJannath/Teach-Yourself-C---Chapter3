95fd3c0877001f45a6cf7e5617f4e271998a9ab5
Tashfia Jannath <76718979+TashfiaJannath@users.noreply.github.com>
2021-03-04 21:02:46 +0800

    Create exa5.c

---
+++ example/exa5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+    int i;
+    char ch;
+    ch='a'; /*give ch an initial value */
+    for(i=0; ch !='q'; i++){
+        printf("pass: %d\n ",i);
+        ch=getche();}
+             return 0;
+             }
